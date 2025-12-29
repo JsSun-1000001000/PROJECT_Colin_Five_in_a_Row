baa23f2350577f38f19d954f9b9dc9916353df92
jssun <59641485@qq.com>
2025-12-30 01:52:51 +0800

    Five in Line Demo, initially commit
    1: design the chessboard;
    
    2: draw the chessboard and chess pieces;
    
    3: finish three significant functions: mouseMoveEvent, mousePressEvent,
    and mouseReleaseEvent;
    
    4: set the background chessboard picture instead of pure colour;
    
    5:what i will do next: write the rule about the game rule judgement;
    
    HAVE A NICE DAY~~ :-P

--- FiveInLineDemo/FiveInLine/fiveinline.cpp
+++ FiveInLineDemo/FiveInLine/fiveinline.cpp
@@ -1,14 +1,197 @@
 #include "fiveinline.h"
 #include "ui_fiveinline.h"
+#include <QPainter>
+#include <QPaintEvent>
+#include <QDebug>
 
 FiveInLine::FiveInLine(QWidget *parent)
     : QWidget(parent)
-    , ui(new Ui::FiveInLine)
+    , ui(new Ui::FiveInLine), m_board( FIL_COLS, std::vector<int>( FIL_ROWS, 0 ) )
+    , m_blackOrWhite(1)
+    , m_movePoint( 0,0 )
+    , m_moveFlag( false )
+
+    //vector 初始化 有参构造 参数 多长 初值多少
 {
     ui->setupUi(this);
+
+    m_pieceColor[None] = QBrush( QColor(0,0,0,0) );//最后一个0透明色
+    m_pieceColor[Black] = QBrush( QColor(0,0,0) );
+    m_pieceColor[White] = QBrush( QColor(255,255,255) );
+
+    //测试数据
+    /*m_board[9][9] = Black;
+    m_board[7][8] = White;
+    m_board[8][8] = Black;*/
+
+    //连接信号和槽
+    connect( &m_timer, SIGNAL( timeout() ),
+             this, SLOT( repaint() ) );
+    m_timer.start(1000/60);//60帧
+
+    //连接落子信号和槽
+    //网络版本 中间有kernel通过kernel发送给服务器 单机版本直接触发
+    connect( this, SIGNAL( SIG_pieceDown( int, int, int ) ),
+             this, SLOT( slot_pieceDown( int, int, int ) ) );
 }
 
 FiveInLine::~FiveInLine()
 {
+    m_timer.stop();
     delete ui;
+}
+//重写绘图事件 绘图背景 棋盘 棋子
+void FiveInLine::paintEvent(QPaintEvent *event)
+{
+    //绘制棋盘
+    QPainter painter(this);//QPainter 用于绘图 使用有参构造 传入当前对象 得到当前控件的画布
+    painter.drawPixmap(FIL_MARGIN_WIDTH  - FIL_DISTANCE,
+                       FIL_MARGIN_HEIGHT - FIL_DISTANCE,
+                       ( FIL_COLS+1 ) * FIL_SPACE + 2 * FIL_DISTANCE,
+                       ( FIL_ROWS+1 ) * FIL_SPACE + 2 * FIL_DISTANCE,
+                       QPixmap(":/plank.jpg"));
+    //painter.setBrush( QBrush( QColor( 255, 160, 0 ) ) );//通过画刷设置颜色
+    /*
+     * 过时了bro 看看我的原木风格棋盘
+     *
+     * painter.drawRect( FIL_MARGIN_WIDTH  - FIL_DISTANCE,
+                      FIL_MARGIN_HEIGHT - FIL_DISTANCE,
+                      ( FIL_COLS+1 ) * FIL_SPACE + 2 * FIL_DISTANCE,
+                      ( FIL_ROWS+1 ) * FIL_SPACE + 2 * FIL_DISTANCE );*/
+    //绘制网格线
+    //竖线
+    for( int i = 1; i <= FIL_COLS; ++i ){
+        painter.setBrush( QBrush( QColor(0,0,0) ) );
+        QPoint p1( FIL_MARGIN_WIDTH + FIL_SPACE * i,
+                   FIL_MARGIN_HEIGHT + FIL_SPACE );
+        QPoint p2( FIL_MARGIN_WIDTH + FIL_SPACE * i,
+                  FIL_MARGIN_HEIGHT + (FIL_ROWS) * FIL_SPACE );
+        painter.drawLine( p1, p2 );
+    }
+    //横线
+    for( int i = 1; i <= FIL_ROWS; ++i ){
+        painter.setBrush( QBrush( QColor(0,0,0) ) );
+        QPoint p1( FIL_MARGIN_WIDTH + FIL_SPACE,
+                   FIL_MARGIN_HEIGHT + FIL_SPACE * i );
+        QPoint p2( FIL_MARGIN_WIDTH + (FIL_COLS) * FIL_SPACE,
+                   FIL_MARGIN_HEIGHT + FIL_SPACE * i );
+        painter.drawLine( p1, p2 );
+    }
+
+    //画中心点
+    painter.setBrush( QBrush( QColor(0,0,0) ) );
+    painter.drawEllipse( QPoint(290,290), 6 / 2, 6 / 2 );
+
+    //绘制棋子
+    for( int x = 0; x < FIL_COLS; ++x ){
+        for( int y = 0; y < FIL_ROWS; ++y ){
+
+            if( m_board[x][y] != None ){
+                painter.setBrush( m_pieceColor[ m_board[x][y] ] );
+                QPoint center( FIL_MARGIN_WIDTH + FIL_SPACE + FIL_SPACE * x ,
+                               FIL_MARGIN_HEIGHT + FIL_SPACE + FIL_SPACE * y );
+                painter.drawEllipse( center, FIL_PIECE_SIZE / 2, FIL_PIECE_SIZE / 2 );
+            }
+        }
+    }
+    //x y点对应的颜色 m_pieceColor[ m_board[x][y] ]
+
+    //绘制移动的棋子 当前回合是谁 画谁
+    if( m_moveFlag ){
+        //根据当前是黑子还是白子 绘制对应颜色的棋子
+        painter.setBrush( m_pieceColor[ getblackOrWhite() ] );
+        QPoint center( m_movePoint.x(),
+                       m_movePoint.y() );
+        painter.drawEllipse( center, FIL_PIECE_SIZE / 2, FIL_PIECE_SIZE / 2 );
+    }
+    event->accept();
+}
+
+void FiveInLine::mousePressEvent(QMouseEvent *event)
+{
+    //捕捉点 相对坐标
+    m_movePoint = event->pos();
+    //位置坐标
+    //相对坐标 相对窗口的
+    //QMouseEvent :: pos();
+    //绝对坐标 相对屏幕的
+    //QCursor::pos(); 鼠标位置
+    //QMouseEvent::globalPos();
+
+    //点击状态
+    m_moveFlag = true;
+    event->accept();
+}
+
+void FiveInLine::mouseMoveEvent(QMouseEvent *event)
+{
+    //捕捉点 相对坐标
+    if(m_moveFlag){
+        m_movePoint = event->pos();
+    }
+    //qDebug() << "mouse move:" << m_movePoint.x() << "," << m_movePoint.y();;
+    event->accept();
+}
+
+void FiveInLine::mouseReleaseEvent(QMouseEvent *event)
+{
+    //释放状态
+    m_moveFlag = false;
+    //计算落子位置 会涉及到取整和四舍五入的问题
+    int x = 0;
+    int y = 0;
+    float fx = (float)event->pos().x();
+    float fy = (float)event->pos().y();
+    fx = ( fx - FIL_MARGIN_WIDTH - FIL_SPACE ) / FIL_SPACE;
+    fy = ( fy - FIL_MARGIN_HEIGHT - FIL_SPACE ) / FIL_SPACE;
+    //类似4.4 4.5 4.6
+    x = ( fx - (int)fx ) > 0.5 ? (int)fx + 1 : (int)fx;
+    y = ( fy - (int)fy ) > 0.5 ? (int)fy + 1 : (int)fy;
+    //是否越界
+    if( isCrossLine( x, y ) ){
+        return;
+    }
+    //发送落子信号
+    Q_EMIT SIG_pieceDown( getblackOrWhite(), x, y );
+
+    //落子
+    event->accept();
+}
+
+int FiveInLine::getblackOrWhite() const
+{
+    return m_blackOrWhite;
+}
+
+void FiveInLine::changeBlackAndWhite()
+{
+    //0,1,2
+    m_blackOrWhite = m_blackOrWhite + 1;
+    if( m_blackOrWhite == 3 ){
+        m_blackOrWhite = Black;
+    }
+}
+
+bool FiveInLine::isCrossLine(int x, int y)
+{
+    if( x < 0   ||
+        x >= 15 ||
+        y < 0   ||
+        y >= 15 ){
+        return true;
+    }
+    return false;
+}
+
+void FiveInLine::slot_pieceDown(int blackorwhite, int x, int y)
+{
+    //更新数组 切换回合
+    if( blackorwhite != getblackOrWhite() ){
+        return;
+    }
+    //落子 没有子才能放
+    if( m_board[x][y] == None ){
+        m_board[x][y] = blackorwhite;
+        changeBlackAndWhite();
+    }
 }
--- FiveInLineDemo/FiveInLine/fiveinline.h
+++ FiveInLineDemo/FiveInLine/fiveinline.h
@@ -9,15 +9,79 @@ class FiveInLine;
 }
 QT_END_NAMESPACE
 
+//界面580*580
+//外边距
+#define FIL_MARGIN_HEIGHT   (50)
+#define FIL_MARGIN_WIDTH    (50)
+//行列数 规范 15*15
+#define FIL_COLS            (15)
+#define FIL_ROWS            (15)
+//边和边之间的边距
+#define FIL_SPACE           (30)
+//棋子大小
+#define FIL_PIECE_SIZE      (28)
+//棋盘边缘缩进的距离
+#define FIL_DISTANCE        (10)
+
+//1.首先绘制棋盘 网格线 棋子
+//2.鼠标点击，出现棋子，然后鼠标移动，棋子跟着移动，鼠标释放，棋子落子
+//3.需要涉及到鼠标点击，鼠标移动，鼠标释放事件
+//4.定时器 定时刷新界面 一秒60帧
+//5.落子 出界 要放弃 合法的 发送信号
+//落子的槽函数 对更新数组 切换回合
+//6.判断输赢
+
+#include <vector>
+#include <QPaintEvent>
+#include <QTimer>
+
 class FiveInLine : public QWidget
 {
     Q_OBJECT
+
+signals://用信号记录落子
+    //落子信号
+    void SIG_pieceDown( int blackorwhite, int x, int y );
 
 public:
     FiveInLine(QWidget *parent = nullptr);
     ~FiveInLine();
 
+    //重写绘图事件 绘图背景 棋盘 棋子
+    void paintEvent(QPaintEvent *event);
+    /*------------------------------------------*/
+    //鼠标点击 出现棋子
+    void mousePressEvent(QMouseEvent *event);
+    //鼠标移动 棋子跟着移动
+    void mouseMoveEvent(QMouseEvent *event);
+    //鼠标释放 棋子落子
+    void mouseReleaseEvent(QMouseEvent *event);
+    //获取当前是黑白回合
+    int getblackOrWhite() const;
+    //切换黑白回合
+    void changeBlackAndWhite();
+    //判断是否出界
+    bool isCrossLine( int x, int y );
+
+public slots:
+    void slot_pieceDown( int blackorwhite, int x, int y );
+
 private:
     Ui::FiveInLine *ui;
+    //记录当前是黑子还是白子回合
+    int m_blackOrWhite;
+    //鼠标移动中
+    QPoint m_movePoint;
+    //移动标志
+    bool m_moveFlag;
+    //黑白子枚举
+    enum ENUM_BLACK_OR_WHITEP{ None = 0, Black = 1, White = 2 };
+    //成员属性 棋盘 二维数组
+    std::vector< std::vector<int> > m_board;
+    //棋子的颜色数组
+    QBrush m_pieceColor[3];
+    //定时器
+    QTimer m_timer;
+
 };
 #endif // FIVEINLINE_H
